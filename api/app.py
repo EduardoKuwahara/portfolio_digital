e80db6bbfcee8d92d45a46a93f7f784f8cccc072
EduardoKuwahara <162118889+EduardoKuwahara@users.noreply.github.com>
2024-05-12 23:15:00 -0300

    arquivos contendo a aplicação e o requirements.txt

---
+++ api/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html')
+@app.route('/informações')
+def informacoes():
+    return render_template('informacoes.html')
+if __name__ == "__main__":
+    app.run(debug=True)
